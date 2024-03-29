f8d45183f6b824e8f993ff60dd61f410e26f475f
Mariana Silva <marir24k@gmail.com>
2024-03-29 00:11:19 -0300

    segundo commit do App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import iconSedans from "./assets/icon-sedans.svg"
+import iconSuvs from "./assets/icon-suvs.svg"
+import iconLuxury from "./assets/icon-luxury.svg"
+
 export default function App(){
   return(
-    <h1>Hello</h1>
+    <main>
+    <section>
+      <div>
+        <img src={iconSedans} alt="icones dos carros sedan" />
+        <h2 className="font-bold">Sedans</h2>
+        <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+        <a href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
+        
+      </div>
+        <img src={iconSuvs} alt="ícone dos carros SUVS" />
+        <h2>SUVs</h2>
+        <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+        <a href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
+      <div>
+
+      <div>
+        <img src={iconLuxury} alt="" />
+        <h2>Luxury</h2>
+        <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+        <a href="https://www.webmotors.com.br/carros-de-luxo" target="_blank">Ver mais</a>
+      </div>
+
+      </div>
+    </section>
+  </main>
   )
 }
